26b24b2b51d7c9a50d6a2d58bfec9e837914884b
Satharin <harasimiuk.rafal@gmail.com>
2017-09-26 09:05:59 +0200

    Delete Config.java

--- Config.java
+++
@@ -1,8 +0,0 @@
-package com.example.rafal.gra;
-
-public class Config {
-    public static final String DATA_URL = "https://mundial2018.000webhostapp.com/getData.php?id=";
-    public static final String KEY_NAME = "name";
-    public static final String KEY_EMAIL = "email";
-    public static final String JSON_ARRAY = "result";
-}
